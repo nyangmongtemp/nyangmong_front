3ce1b3bba571f4d1e577c62b80d8c60ca8bd282b
EunHyeok <secun77@naver.com>
2025-07-28 17:54:15 +0900

    관리자 답변 노출 이름 수정

--- src/components/CustomerInquiry.jsx
+++ src/components/CustomerInquiry.jsx
@@ -260,12 +260,12 @@ const CustomerInquiry = () => {
               >
                 <SelectTrigger className="w-32 h-9 text-sm">
                   <SelectValue placeholder="정렬" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="latest">최신순</SelectItem>
-              <SelectItem value="oldest">오래된순</SelectItem>
-            </SelectContent>
-          </Select>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="latest">최신순</SelectItem>
+                  <SelectItem value="oldest">오래된순</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
           {/* 문의 리스트 */}
@@ -374,8 +374,8 @@ const CustomerInquiry = () => {
                   {createLoading ? "등록 중..." : "등록하기"}
                 </Button>
               </div>
-        </div>
-      </div>
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
 
@@ -492,10 +492,7 @@ const CustomerInquiry = () => {
                     <div className="mt-6 p-4 rounded-lg bg-gradient-to-r from-orange-50 to-pink-50 border border-orange-200">
                       <div className="mb-2">
                         <span className="font-semibold text-gray-700">
-                          관리자 ID
-                        </span>
-                        <span className="ml-2 text-gray-800">
-                          {detailData.adminId ?? "-"}
+                          관리자
                         </span>
                       </div>
                       <div>
@@ -526,7 +523,7 @@ const CustomerInquiry = () => {
                 </>
               ) : null}
             </div>
-      </div>
+          </div>
         </DialogContent>
       </Dialog>
     </div>
