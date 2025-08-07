566e5f3842223be37592e5f21aaf5b7dc25df5b4
EunHyeok <secun77@naver.com>
2025-08-07 17:38:22 +0900

    timeout 늘리기

--- configs/axios-config.js
+++ configs/axios-config.js
@@ -22,7 +22,7 @@ const axiosInstance = axios.create({
     // "Content-Type": "application/json",
   },
   withCredentials: true,
-  timeout: 15000, // 10초 타임아웃 설정
+  timeout: 30000, // 10초 타임아웃 설정
 });
 
 // ✅ 요청 인터셉터 설정
